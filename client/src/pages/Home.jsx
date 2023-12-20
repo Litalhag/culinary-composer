8b97b0b6e7c5473771aabfcd759227845391b11f
Litalhag <litalhag@gmail.com>
2023-12-20 15:48:01 +0200

    sharedlayout, pages

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,8 @@
+import Footer from '../components/Footer'
+import NavHead from '../components/navbar/NavHead'
+import Footer from '../components/Footer'
+
+const Home = () => {
+  return <NavHead />
+}
+export default Home
