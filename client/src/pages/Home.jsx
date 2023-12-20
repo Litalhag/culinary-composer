32b127d9232450c72e1e92b9b9755f67f3ecccea
Litalhag <litalhag@gmail.com>
2023-12-20 17:26:05 +0200

    form

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import Showcase from '../components/Showcase'
+import IngredientAutocomplete from '../components/IngredientAutocomplete'
+import KitchenAppliancesSelect from '../components/KitchenAppliancesSelect'
+import PeopleNumberInput from '../components/PeopleNumberInput'
 const Home = () => {
   return (
     <>
       <Showcase />
+      <IngredientAutocomplete />
+      <KitchenAppliancesSelect />
+      <PeopleNumberInput />
     </>
   )
 }
